f303e28678cedc661fd69de4797b8f1c88a4bf83
David Caldwell <david@avvitt.com>
2019-09-15 04:27:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,6 +15,9 @@ def consolidate_cart(cart)
   new_cart
 end
 
+
+
+
 def apply_coupons(cart, coupons)
   #doesn't break if there is no coupon
   return cart if coupons == []
@@ -45,16 +48,6 @@ def apply_coupons(cart, coupons)
    new_cart
 end
 
-#result:
-{
-  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
-  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
-  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
-}
-
-# apply clearance:
-# takes 20% off price if the item is on clearance
-# does not discount the price for items not on clearance
 
 def apply_clearance(cart)
   new_cart = cart
@@ -66,17 +59,10 @@ def apply_clearance(cart)
   new_cart #if not, just return the same cart
 end
 
-# checkout:
-# Apply coupon discounts if the proper number of items are present.# calls on #apply_clearance after calling on #apply_coupons when there is only one item in the cart and no coupon
-# Apply 20% discount if items are on clearance.
-# If, after applying the coupon discounts and the clearance discounts, the cart's total is over $100, then apply a 10% discount.
 
 def checkout(cart, coupons)
-  #call the consolidate to get the count item first
   new_cart = consolidate_cart(cart)
-  #apply coupon to the new cart
   apply_coupons(new_cart, coupons)
-  #apply clearance after discount from coupon
   apply_clearance(new_cart)
 
 total = 0
