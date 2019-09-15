b643654646b4698809db5632b132f4a1b4930f63
David Caldwell <david@avvitt.com>
2019-09-15 04:33:29 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,5 +1,24 @@
 require_relative 'grocer'
 
+def generateCart
+#	cart = []
+#	rand(20).times do
+#		cart.push(ITEMS.sample)  
+#	end
+#	cart
+#end
+#
+##randomly generates set of coupons
+#def generateCoups
+#	coups = []
+#	rand(2).times do
+#		coups.push(COUPS.sample)
+#	end
+#	coups
+#end
+
+
+
 def consolidate_cart(cart)
   hash = {}
   cart.each do |item_hash|
