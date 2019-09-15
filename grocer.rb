821d6b0a497b4f8447f72cc220121d3be3daed36
David Caldwell <david@avvitt.com>
2019-09-15 04:28:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,12 @@
 def consolidate_cart(cart)
   new_cart = {}
-  cart.each do |hash| #hash is the whole array
-    hash.each do |name, describe| #name: avocado, cheese. describe: price, clearance
-      #if new_cart has name and count already, increase the count
+  cart.each do |hash| 
+    hash.each do |name, describe| 
       if new_cart[name]
         new_cart[name][:count] += 1
-      #new_cart is empty so set name as key and describe as value
       else
         new_cart[name] = describe
-        new_cart[name][:count] = 1 #set count = 1 cuz we set name and describe for 1 item
+        new_cart[name][:count] = 1
       end
     end
   end
@@ -22,20 +20,17 @@ def apply_coupons(cart, coupons)
   #doesn't break if there is no coupon
   return cart if coupons == []
 
-  #set new_cart = cart so we don't have to push all the values, just change them
   new_cart = cart
 
   coupons.each do |coupon|
-    name = coupon[:item] #avocado, cheese,...
+    name = coupon[:item] 
     num_of_c = coupon[:num]
     #if the cart has the same item in coupon and has larger amount than in coupon
     if cart.include?(name) && cart[name][:count] >= num_of_c
        #remove number of the new_cart's count
        new_cart[name][:count] -= num_of_c
-       #increase the count when there is more items than the coupon allows
        if new_cart["#{name} W/COUPON"]
          new_cart["#{name} W/COUPON"][:count] += 1
-       #set the name with coupon with new value
        else
          new_cart["#{name} W/COUPON"] = {
            :price => coupon[:cost],
@@ -52,11 +47,11 @@ end
 def apply_clearance(cart)
   new_cart = cart
   cart.each do |name, hash|
-      if hash[:clearance] #if clearance is true, take 20% off
+      if hash[:clearance] 
         new_cart[name][:price] = (cart[name][:price] * 0.8).round(2)
       end
   end
-  new_cart #if not, just return the same cart
+  new_cart 
 end
 
 
