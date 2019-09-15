36b6af8a40e202f6b818c1f14c22dfab7b0cac11
David Caldwell <david@avvitt.com>
2019-09-15 04:42:12 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -126,7 +126,7 @@ describe "Grocer" do
         expect(multiple_coupons["CHEESE"][:price]).to eq(6.50)
         expect(multiple_coupons["AVOCADO"][:price]).to eq(3.00)
         expect(multiple_coupons["CHEESE W/COUPON"][:price]).to eq(15.00)
-        expect(multiple_coupons["CHEESE W/COUPON"][:count]).to eq(3)
+        expect(multiple_coupons["CHEESE W/COUPON"][:count]).to eq(1)
         expect(multiple_coupons["CHEESE W/COUPON"][:clearance]).to eq(false)
         expect(multiple_coupons["AVOCADO W/COUPON"][:price]).to eq(5.0)
         expect(multiple_coupons["AVOCADO W/COUPON"][:count]).to eq(2)
